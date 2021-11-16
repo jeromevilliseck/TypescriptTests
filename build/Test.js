4512f6a010eba6cd3c30c1bde99d83f712f604b0
villiseck <jvk@abes.fr>
2021-11-16 14:51:23 +0100

    Currifycation niveau 2

--- build/Test.js
+++ build/Test.js
@@ -1,14 +1,24 @@
-var Animal = /** @class */ (function () {
-    function Animal(name) {
-        this.name = name;
-    }
-    return Animal;
-}());
 var Main = /** @class */ (function () {
     function Main() {
-        console.log('Démarrage Page de test');
+        console.log(tabDouble(tab));
+        var triple = tabMultiply(3);
+        console.log(triple(tab));
+        console.log(tabMultiplyLambda(4)(tab));
     }
     return Main;
 }());
+var tab = [1, 5, 2, 4, 3];
+//Programmation fonctionnelle avec lambda
+function tabDouble(inputTable) {
+    return inputTable.map(function (v) { return v * 2; });
+}
+//Curryfication classique : retour de fonction
+function tabMultiply(multiple) {
+    return function (tabEntry) {
+        return tabEntry.map(function (v) { return v * multiple; });
+    };
+}
+//Curryfication avancée : lambda
+var tabMultiplyLambda = function (m) { return function (t) { return t.map(function (x) { return x * m; }); }; };
 var main = new Main();
 //# sourceMappingURL=Test.js.map
--- src/scripts/Test.ts
+++ src/scripts/Test.ts
@@ -1,7 +1,27 @@
 class Main {
     constructor() {
-        console.log('Démarrage Page de test')
+        console.log(tabDouble(tab));
+        let triple = tabMultiply(3);
+        console.log(triple(tab));
+        console.log(tabMultiplyLambda(4)(tab));
     }
 }
+
+let tab : number[] = [1,5,2,4,3];
+
+//Programmation fonctionnelle avec lambda
+function tabDouble(inputTable: Array<number>): Array<number>{
+    return inputTable.map(v => v*2);
+}
+
+//Curryfication classique : retour de fonction
+function tabMultiply(multiple: number){
+    return function(tabEntry: Array<number>){
+        return tabEntry.map(v => v*multiple);
+    }
+}
+
+//Curryfication avancée : lambda
+let tabMultiplyLambda = (m: number) => (t: Array<number>) => t.map(x => x*m);
 
 var main: Main = new Main();
